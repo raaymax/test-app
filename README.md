23c09abbf70f2c346207e355bf1f263f18468932
Mateusz Russak <mateusz@russak.biz>
2024-04-25 15:28:13 +0200

    feat: initialized app

---
+++ README.md
@@ -0,0 +1,9 @@
+# Test app
+
+
+## Startup 
+
+```bash
+poetry install
+poetry run streamsync run .
+```
---
+++ main.py
@@ -0,0 +1,7 @@
+import streamsync as ss
+
+
+initial_state = ss.init_state({
+    "app_name": "Some minimal app",
+})
+
